7e233fb50045f9b7269bb87b4dcb59de46385e7c
fsdfsd <fsdfsd>
2019-10-24 09:29:34 +0300

    marafon

---
+++ MarATHON/newrunner1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarATHON
+{
+    public partial class newrunner1 : Form
+    {
+        public newrunner1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Mains open = new Mains();
+            open.Show();
+            this.Close();
+        }
+    }
+}
